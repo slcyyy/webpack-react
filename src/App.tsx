b3065f08d904f0b1121ce97f241a348c239ac5af
luochun <luochun@idcos.com>
2023-05-31 10:46:39 +0800

    feat: 代码规范prettier & eslint & husky & lint

--- src/App.tsx
+++ src/App.tsx
@@ -1,42 +1,42 @@
-import { useEffect, useState } from "react";
-import { useSelector } from "react-redux";
-import { selectUsersData } from "./store/modules/user";
-import { fetchUsers, addUser } from "@/store/modules/user";
+import { useEffect, useState } from 'react'
+import { useSelector } from 'react-redux'
+import { selectUsersData } from './store/modules/user'
+import { fetchUsers, addUser } from '@/store/modules/user'
 
-import { useAppDispatch } from "./store";
-import { nanoid } from "@reduxjs/toolkit";
-import router from "./routes";
-import "antd/dist/reset.css";
-import { RouterProvider } from "react-router-dom";
+import { useAppDispatch } from './store'
+import { nanoid } from '@reduxjs/toolkit'
+import router from './routes'
+import 'antd/dist/reset.css'
+import { RouterProvider } from 'react-router-dom'
 
 function App() {
-  const usersData = useSelector(selectUsersData);
-  const { users, status } = usersData;
-  const dispatch = useAppDispatch();
+  const usersData = useSelector(selectUsersData)
+  const { users, status } = usersData
+  const dispatch = useAppDispatch()
 
-  const [username, setUsername] = useState("");
-  const [email, setEmail] = useState("");
-  const [sex, setSex] = useState("");
+  const [username, setUsername] = useState('')
+  const [email, setEmail] = useState('')
+  const [sex, setSex] = useState('')
 
   useEffect(() => {
     // dispatch(fetchUsers());
-  }, []);
+  }, [])
 
   const addUserEvent = async (e: any) => {
     // 防止页面刷新？
-    e.preventDefault();
+    e.preventDefault()
 
     const param = {
       id: nanoid(),
       name: username,
       email: email,
       sex: sex,
-    };
-    await dispatch(addUser(param));
-    setUsername("");
-    setEmail("");
-    setSex("");
-  };
+    }
+    await dispatch(addUser(param))
+    setUsername('')
+    setEmail('')
+    setSex('')
+  }
 
   return (
     <RouterProvider router={router} />
@@ -72,7 +72,7 @@ function App() {
     //     <button onClick={addUserEvent}>提交</button>
     //   </form>
     // </>
-  );
+  )
 }
 
-export default App;
+export default App
